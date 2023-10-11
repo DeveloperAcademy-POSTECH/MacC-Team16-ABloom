33b134bf27b636f4050d744e57739f16bb559f44
Hayun218 <gkahsdl@icloud.com>
2023-10-11 15:19:39 +0900

    [Init] 초기셋팅프로젝트

---
+++ ABloom/ABloom/ABloomApp.swift
@@ -0,0 +1,21 @@
+//
+//  ABloomApp.swift
+//  ABloom
+//
+//  Created by yun on 10/11/23.
+//
+import SwiftUI
+
+import ComposableArchitecture
+
+@main
+struct ABloomApp: App {
+  static let store = Store(initialState: MainViewFeature.State()) {
+    MainViewFeature()
+  }
+  var body: some Scene {
+        WindowGroup {
+          MainView(store: ABloomApp.store)
+        }
+    }
+}
