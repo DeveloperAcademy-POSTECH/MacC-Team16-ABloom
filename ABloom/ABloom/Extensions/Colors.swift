847b674341a512fd75a69edbdbe08c2225231f14
Hayun218 <gkahsdl@icloud.com>
2023-10-18 00:24:36 +0900

    [Design] 디자인 시스템 적용1

---
+++ ABloom/ABloom/Extensions/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  ABloom
+//
+//  Created by yun on 10/17/23.
+//
+
+import SwiftUI
+
+extension Color {
+  init(hex: UInt, alpha: Double = 1) {
+    self.init(
+      .sRGB,
+      red: Double((hex >> 16) & 0xff) / 255,
+      green: Double((hex >> 08) & 0xff) / 255,
+      blue: Double((hex >> 00) & 0xff) / 255,
+      opacity: alpha
+    )
+  }
+}
