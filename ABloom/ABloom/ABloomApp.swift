16f959a312e899a492b289bed342a498575ee182
radin <dlwlsgml8847@gmail.com>
2023-10-17 12:09:08 +0900

    [Feat/#5] 들여쓰기 수정

--- ABloom/ABloom/ABloomApp.swift
+++ ABloom/ABloom/ABloomApp.swift
@@ -11,7 +11,7 @@ struct ABloomApp: App {
   
   var body: some Scene {
     WindowGroup {
-			TabBarView()
+      TabBarView()
     }
   }
 }
--- ABloom/ABloom/TabBar/Tab.swift
+++ ABloom/ABloom/TabBar/Tab.swift
@@ -6,41 +6,41 @@
 //
 
 enum TabTitle: String {
-	case main = "메인"
-	case qna = "문답"
-	case info = "내 정보"
+  case main = "메인"
+  case qna = "문답"
+  case info = "내 정보"
 }
 
 enum TabIcon: String {
-	case main = "house.fill"
-	case qna = "doc.plaintext.fill"
-	case info = "person.fill"
+  case main = "house.fill"
+  case qna = "doc.plaintext.fill"
+  case info = "person.fill"
 }
 
 enum Tab: CaseIterable {
-	case main
-	case qna
-	case info
-	
-	var icon: String {
-		switch self {
-		case .main:
-			return TabIcon.main.rawValue
-		case .qna:
-			return TabIcon.qna.rawValue
-		case .info:
-			return TabIcon.info.rawValue
-		}
-	}
-	
-	var title: String {
-		switch self {
-		case .main:
-			return TabTitle.main.rawValue
-		case .qna:
-			return TabTitle.qna.rawValue
-		case .info:
-			return TabTitle.info.rawValue
-		}
-	}
+  case main
+  case qna
+  case info
+  
+  var icon: String {
+    switch self {
+    case .main:
+      return TabIcon.main.rawValue
+    case .qna:
+      return TabIcon.qna.rawValue
+    case .info:
+      return TabIcon.info.rawValue
+    }
+  }
+  
+  var title: String {
+    switch self {
+    case .main:
+      return TabTitle.main.rawValue
+    case .qna:
+      return TabTitle.qna.rawValue
+    case .info:
+      return TabTitle.info.rawValue
+    }
+  }
 }
