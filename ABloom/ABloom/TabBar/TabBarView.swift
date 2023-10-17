d7f89d65d5496e87b54d0f77d466b27734d16627
radin <dlwlsgml8847@gmail.com>
2023-10-17 12:09:08 +0900

    [Feat/#5] 들여쓰기 수정

--- ABloom/ABloom/TabBar/TabBarView.swift
+++ ABloom/ABloom/TabBar/TabBarView.swift
@@ -8,39 +8,39 @@
 import SwiftUI
 
 struct TabBarView: View {
-	@State var selectedTab: Tab = .main
-	
-	var body: some View {
-		TabView(selection: $selectedTab) {
-			ForEach(Tab.allCases, id: \.self) { tab in
-				switch tab {
-				case .main:
-					Text("메인뷰")
-						.tabItem {
-							Image(systemName: tab.icon)
-							Text(tab.title)
-						}
-						.tag(tab)
-				case .qna:
-					Text("문답뷰")
-						.tabItem {
-							Image(systemName: tab.icon)
-							Text(tab.title)
-						}
-						.tag(tab)
-				case .info:
-					Text("내정보뷰")
-						.tabItem {
-							Image(systemName: tab.icon)
-							Text(tab.title)
-						}
-						.tag(tab)
-				}
-			}
-		}
-	}
+  @State var selectedTab: Tab = .main
+  
+  var body: some View {
+    TabView(selection: $selectedTab) {
+      ForEach(Tab.allCases, id: \.self) { tab in
+        switch tab {
+        case .main:
+          Text("메인뷰")
+            .tabItem {
+              Image(systemName: tab.icon)
+              Text(tab.title)
+            }
+            .tag(tab)
+        case .qna:
+          Text("문답뷰")
+            .tabItem {
+              Image(systemName: tab.icon)
+              Text(tab.title)
+            }
+            .tag(tab)
+        case .info:
+          Text("내정보뷰")
+            .tabItem {
+              Image(systemName: tab.icon)
+              Text(tab.title)
+            }
+            .tag(tab)
+        }
+      }
+    }
+  }
 }
 
 #Preview {
-	TabBarView()
+  TabBarView()
 }
