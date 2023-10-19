b4b5eb5bb6d198d6a74ffdba66bc18f5e75f2415
seunggyun-jeong <jskyun98@gmail.com>
2023-10-19 17:10:25 +0900

    [Chore/#12] Apple login View Representable 주석 추가

--- ABloom/ABloom/Login/SubViews/AppleLoginButtonViewRepresentable.swift
+++ ABloom/ABloom/Login/SubViews/AppleLoginButtonViewRepresentable.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 import AuthenticationServices
 
+/// 애플에 내장된 로그인 버튼을 구현할 수 있는 UIViewRepresentable입니다.
+///
+/// - Parameters:
+///     - type: 버튼의 안내 메세지를 설정합니다.
+///     - style: 버튼의 스타일을 설정합니다.
+/// - Returns: Apple 로그인 버튼을 표시합니다.
+///
+/// ```swift
+/// // 버튼 생성 예시
+/// SignInWithAppleButtonViewRepresentable(type: .continue, style: .black)
+/// ```
 struct SignInWithAppleButtonViewRepresentable: UIViewRepresentable {
   
   let type: ASAuthorizationAppleIDButton.ButtonType
