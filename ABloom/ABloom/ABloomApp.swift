8550e1cbbdee63a4dbd20669de63875734d37ce7
seunggyun-jeong <jskyun98@gmail.com>
2023-10-19 10:31:24 +0900

    [Add/#12] 파이어베이스 연동

--- ABloom/ABloom/ABloomApp.swift
+++ ABloom/ABloom/ABloomApp.swift
@@ -5,9 +5,15 @@
 //  Created by yun on 10/11/23.
 //
 import SwiftUI
+import FirebaseCore
 
 @main
 struct ABloomApp: App {
+  
+  init() {
+    FirebaseApp.configure()
+    print("Firebase configured!")
+  }
   
   var body: some Scene {
     WindowGroup {
