a54d2104aa9413b9799598c5184c3d567ee1d5fb
seunggyun-jeong <jskyun98@gmail.com>
2023-10-22 14:12:23 +0900

    [Fix/#28] ViewExtension - defaultBackground 이름 수정

---
+++ ABloom/ABloom/Menu/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  ABloom
+//
+//  Created by 정승균 on 10/22/23.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
