14d24b061306800197c99b7d089a6cb2d41ce7a0
radin <dlwlsgml8847@gmail.com>
2023-10-26 00:44:20 +0900

    [Feat/#31] MyAccountConnectingView, ViewModel 수정

--- ABloom/ABloom/Presentation/CoreViews/Menu/SubViews/MyAccountConnecting/MyAccountConnectingViewModel.swift
+++ ABloom/ABloom/Presentation/CoreViews/Menu/SubViews/MyAccountConnecting/MyAccountConnectingViewModel.swift
@@ -5,21 +5,44 @@
 //  Created by 정승균 on 10/22/23.
 //
 
-import Foundation
+import SwiftUI
 
-@MainActor
 final class MyAccountConnectingViewModel: ObservableObject {
   @Published var codeInputText: String = ""
   @Published var myCode: String?
+  @Published var isConnectAble: Bool = false
   @Published var showToast = false
+  @Published var showAlert = false
   
-  var isTargetCodeVaild: Bool {
+  // TODO: 연결시도가능, 유효가능 구분
+  var isTargetCodeInputVaild: Bool {
     !codeInputText.isEmpty
+  }
+  
+  // TODO: 연결 가능 확인 로직 수정
+  func tryConnect() {
+    self.isConnectAble = isTargetCodeInputVaild && codeInputText.count == 3
+    if !isConnectAble {
+      self.showAlert = true
+    }
   }
   
   func getMyCode() async throws {
     let currentUser = try AuthenticationManager.shared.getAuthenticatedUser()
     print("현재 유저 -> \(currentUser)")
     self.myCode = try await UserManager.shared.getUser(userId: currentUser.uid).invitationCode
+  }
+  
+  func copyClipboard() {
+    UIPasteboard.general.string = myCode
+    withAnimation {
+      showToast.toggle()
+    }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      withAnimation {
+        self.showToast = false
+      }
+    }
   }
 }
