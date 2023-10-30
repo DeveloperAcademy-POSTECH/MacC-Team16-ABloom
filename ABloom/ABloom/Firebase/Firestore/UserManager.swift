df218b2912da261fe5438fe47537e99c6e5a25c3
seunggyun-jeong <jskyun98@gmail.com>
2023-10-30 20:07:34 +0900

    [Feat/#93] 상대방과 함께 작성한 문항 개수 홈에 표시

--- ABloom/ABloom/Firebase/Firestore/UserManager.swift
+++ ABloom/ABloom/Firebase/Firestore/UserManager.swift
@@ -82,13 +82,21 @@ final class UserManager {
     return try await collection.getDocuments(as: DBAnswer.self)
   }
   
+  func getAnswerWithId(userId: String, filter: [Any]) async throws -> [DBAnswer] {
+    let collection = userAnswerCollection(userId: userId)
+    
+    return try await collection
+      .whereField(DBAnswer.CodingKeys.questionId.rawValue, in: filter)
+      .getDocuments(as: DBAnswer.self)
+  }
+  
   func getAnswer(userId: String, questionId: Int) async throws -> DBAnswer {
     let collection = userAnswerCollection(userId: userId)
-    let userAnsewer = try await collection
+    let userAnswer = try await collection
       .whereField(DBAnswer.CodingKeys.questionId.rawValue, isEqualTo: questionId)
       .getDocuments(as: DBAnswer.self)
     
-    guard let answer = userAnsewer.first else { throw URLError(.badServerResponse)}
+    guard let answer = userAnswer.first else { throw URLError(.badServerResponse)}
     
     return answer
   }
