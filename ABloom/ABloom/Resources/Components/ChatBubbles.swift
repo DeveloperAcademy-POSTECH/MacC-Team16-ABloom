3275010caf53daa62bfa3724dcea2e85734d0e6f
Hayun218 <gkahsdl@icloud.com>
2023-11-06 22:57:32 +0900

    [Design/#163] 컴포넌트 1차 적용

--- ABloom/ABloom/Resources/Components/ChatBubbles.swift
+++ ABloom/ABloom/Resources/Components/ChatBubbles.swift
@@ -10,6 +10,8 @@ import SwiftUI
 // Customized Chat Bubbles
 
 let paddingV = 10.0
+let paddingH = 12.0
+let conerV = 12.0
 
 struct LeftChatBubble: View {
   let text: String
@@ -33,14 +35,15 @@ struct LeftChatBubble: View {
       Spacer()
       
       Text(text)
-        .fontWithTracking(isBold ? .footnoteBold : .footnoteR, tracking: -0.4)
+        .fontWithTracking(isBold ? .chatBubbleBtn : .chatBubble, tracking: -0.2)
         .foregroundStyle(.stone900)
-        .padding(paddingV)
+        .padding(.vertical, paddingV)
+        .padding(.horizontal, paddingH)
         .multilineTextAlignment(.leading)
         .background(
           Rectangle()
             .foregroundStyle(isMale ? .blue100 : .pink100)
-            .cornerRadius(10, corners: [.topRight, .bottomRight, .bottomLeft])
+            .cornerRadius(conerV, corners: [.topRight, .bottomRight, .bottomLeft])
         )
         .frame(minHeight: 20, alignment: .center)
         .frame(maxWidth: .infinity, alignment: .leading)
@@ -72,14 +75,15 @@ struct RightChatBubble: View {
       Spacer()
       
       Text(text)
-        .fontWithTracking(isBold ? .footnoteBold : .footnoteR, tracking: -0.4)
+        .fontWithTracking(isBold ? .chatBubbleBtn : .chatBubble, tracking: -0.2)
         .foregroundStyle(.stone900)
         .multilineTextAlignment(.leading)
-        .padding(paddingV)
+        .padding(.vertical, paddingV)
+        .padding(.horizontal, paddingH)
         .background(
           Rectangle()
             .foregroundStyle(self.isMale ? .blue100 : .pink100)
-            .cornerRadius(10, corners: [.topLeft, .bottomRight, .bottomLeft])
+            .cornerRadius(conerV, corners: [.topLeft, .bottomRight, .bottomLeft])
         )
         .frame(minHeight: 16, alignment: .center)
         .frame(maxWidth: 270, alignment: .trailing)
@@ -114,23 +118,77 @@ struct ChatBubbleTextField: View {
     TextField("", text: $text, axis: .vertical)
       .placeholder(when: text.isEmpty, placeholder: {
         Text("답변을 작성해주세요. (150자 이내)")
-          .font(.custom("SpoqaHanSansNeo-Regular", size: 12))
-          .tracking(-0.4)
+          .font(.custom("SpoqaHanSansNeo-Regular", size: 14))
+          .tracking(-0.2)
           .foregroundStyle(.stone300)
       })
-    
-      .font(.custom("SpoqaHanSansNeo-Regular", size: 12))
-      .tracking(-0.4)
+      .font(.custom("SpoqaHanSansNeo-Regular", size: 14))
+      .tracking(-0.2)
       .foregroundStyle(.white)
       .multilineTextAlignment(.leading)
-      .padding(paddingV)
+      .padding(.vertical, paddingV)
+      .padding(.horizontal, paddingH)
       .background(
         Rectangle()
           .clayMorpMDShadow()
           .foregroundStyle(self.isMale ? .blue500 : .pink500)
-          .cornerRadius(10, corners: [.topLeft, .bottomRight, .bottomLeft])
+          .cornerRadius(conerV, corners: [.topLeft, .bottomRight, .bottomLeft])
       )
       .frame(minHeight: 30, alignment: .center)
       .frame(width: 242, alignment: .center)
+  }
+}
+
+// MARK: - Chat Callout
+
+struct ChatCallout: View {
+  let text: String
+  
+  var body: some View {
+    Text(text)
+      .fontWithTracking(.caption2R, tracking: -0.4)
+      .foregroundStyle(.white)
+      .padding(.vertical, 7)
+      .padding(.horizontal, 14)
+      .background(
+        RoundedRectangle(cornerRadius: 24)
+          .foregroundStyle(.black.opacity(0.3))
+      )
+  }
+}
+
+// MARK: - ChatBubbleBtn
+struct ChatBubbleBtn: View {
+  let text: String
+  let isMale: Bool
+  
+  var body: some View {
+    HStack {
+      Spacer()
+      
+      Text(text)
+        .fontWithTracking(.chatBubbleBtn, tracking: -0.2)
+        .foregroundStyle(.stone900)
+        .multilineTextAlignment(.leading)
+        .padding(.vertical, paddingV)
+        .padding(.horizontal, paddingH)
+        .background(
+          Rectangle()
+            .foregroundStyle(self.isMale ? .blue100 : .pink100)
+            .cornerRadius(conerV, corners: [.topLeft, .bottomRight, .bottomLeft])
+        )
+        .frame(minHeight: 16, alignment: .center)
+        .frame(maxWidth: 270, alignment: .trailing)
+    }
+  }
+}
+
+
+#Preview {
+  Group {
+    LeftChatBubble(text: "내용을 입력해주세요.", isMale: true)
+    RightChatBubble(text: "내용을 입력해주세요.", isMale: false)
+    ChatCallout(text: "내용")
+    
   }
 }
