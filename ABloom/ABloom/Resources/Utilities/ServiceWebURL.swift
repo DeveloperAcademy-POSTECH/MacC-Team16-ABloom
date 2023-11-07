47857a126d50eec7a4e33c4c27151d84cef25f41
seunggyun-jeong <jskyun98@gmail.com>
2023-11-07 10:33:29 +0900

    [Bug] 고객 센터 URL 수정

--- ABloom/ABloom/Resources/Utilities/ServiceWebURL.swift
+++ ABloom/ABloom/Resources/Utilities/ServiceWebURL.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum ServiceWebURL: String {
   case questionLab = "https://bit.ly/3FKGoKB"
-  case qna = "https://bit.ly/3Sq7A8S"
+  case qna = "https://bit.ly/3tUvUpd"
   case termsOfuse = "https://jaeseoklee.notion.site/9a4458152dd045ca82f8010fb46c5776"
   case privacyPolicy = "https://jaeseoklee.notion.site/Mery-78953ca6310b40209cb993312bbf9339?pvs=4"
 }
