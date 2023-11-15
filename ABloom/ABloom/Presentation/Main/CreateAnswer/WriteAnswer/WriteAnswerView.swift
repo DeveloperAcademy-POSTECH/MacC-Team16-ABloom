e45e1a844b0303b97fba20dca8a4ccf66170f2b6
seunggyun-jeong <jskyun98@gmail.com>
2023-11-15 10:22:56 +0900

    [Refactor] 리팩토링을 위한 준비

---
+++ ABloom/ABloom/Presentation/Main/CreateAnswer/WriteAnswer/WriteAnswerView.swift
@@ -0,0 +1,18 @@
+//
+//  WriteAnswerView.swift
+//  ABloom
+//
+//  Created by 정승균 on 11/15/23.
+//
+
+import SwiftUI
+
+struct WriteAnswerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WriteAnswerView()
+}
---
+++ ABloom/ABloom/Presentation/SignIn/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  ABloom
+//
+//  Created by 정승균 on 11/15/23.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
