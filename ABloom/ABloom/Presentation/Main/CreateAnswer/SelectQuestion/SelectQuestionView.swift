0c84eb9740f009d5da3f5aa7bfdfa3a82801a86f
seunggyun-jeong <jskyun98@gmail.com>
2023-11-17 15:56:51 +0900

    [Ref/#224] 파일명 처리

---
+++ ABloom/ABloom/Presentation/Main/CreateAnswer/SelectQuestion/SelectQuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectQuestionView.swift
+//  ABloom
+//
+//  Created by 정승균 on 11/15/23.
+//
+
+import SwiftUI
+
+struct SelectQuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectQuestionView()
+}
---
+++ ABloom/ABloom/Presentation/Main/Menu/Connection/ConnectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectionView.swift
+//  ABloom
+//
+//  Created by 정승균 on 11/15/23.
+//
+
+import SwiftUI
+
+struct ConnectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+  ConnectionView()
+}
