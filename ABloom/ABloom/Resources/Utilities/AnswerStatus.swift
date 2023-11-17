bca0406b5683924fd9a9d3d394847f77e886ee9a
radin <dlwlsgml8847@gmail.com>
2023-11-17 22:22:22 +0900

    [Design/#233] AnswerStatus 수정(case, 상태별 변수 수정)

--- ABloom/ABloom/Resources/Utilities/AnswerStatus.swift
+++ ABloom/ABloom/Resources/Utilities/AnswerStatus.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 enum AnswerStatus {
-  case both
   case onlyMe
   case onlyFinace
-  case nobody
+  case reactOnlyMe
+  case reactOnlyFinace
   case completed
   
   var text: String {
     switch self {
-    case .both:
-      return "완성해주세요 >"
     case .onlyMe:
       return "답변을 기다리고 있어요"
     case .onlyFinace:
-      return "답변해주세요 >"
-    case .nobody:
-      return ""
+      return "답변해주세요  >"
+    case .reactOnlyMe:
+      return "반응을 기다리고 있어요"
+    case .reactOnlyFinace:
+      return "반응해주세요  >"
     case .completed:
       return " 완성된 문답"
     }
@@ -31,30 +31,21 @@ enum AnswerStatus {
   
   var textColor: Color {
     switch self {
-    case .both, .onlyMe, .onlyFinace, .nobody:
-      return .stone50
-    case .completed:
-      return .purple600
+    case .onlyMe, .reactOnlyMe:
+      return .gray600
+    case .completed, .onlyFinace, .reactOnlyFinace:
+      return .white
     }
   }
   
   var backgroundColor: Color {
     switch self {
-    case .onlyMe:
-      return .purple500
-    case .both, .onlyFinace:
-      return .purple600
-    case .completed, .nobody:
-      return .white
-    }
-  }
-
-  var image: Image? {
-    switch self {
     case .completed:
-      return Image(systemName: "checkmark")
-    case .both, .onlyMe, .onlyFinace, .nobody:
-      return nil
+      return .complete
+    case .onlyMe, .reactOnlyMe:
+      return .white
+    case .onlyFinace, .reactOnlyFinace:
+      return .purple600
     }
   }
 }
