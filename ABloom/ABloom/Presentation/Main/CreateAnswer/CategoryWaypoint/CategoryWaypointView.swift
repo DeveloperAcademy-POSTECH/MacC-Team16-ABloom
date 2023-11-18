00877e3d464e8421b68e52752605759eb060f9b4
Hayun218 <gkahsdl@icloud.com>
2023-11-18 23:14:05 +0900

    [Design/#236] createQuestionView 구현 및 에셋 추가

--- ABloom/ABloom/Presentation/Main/CreateAnswer/CategoryWaypoint/CategoryWaypointView.swift
+++ ABloom/ABloom/Presentation/Main/CreateAnswer/CategoryWaypoint/CategoryWaypointView.swift
@@ -8,11 +8,77 @@
 import SwiftUI
 
 struct CategoryWaypointView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @StateObject var categoryWayVM = CategoryWaypointViewModel()
+  
+  var body: some View {
+    
+    ScrollView {
+      
+      VStack(alignment: .leading, spacing: 35) {
+        
+        recommenedArea
+          .padding(.top, 20)
+        
+        Text("카테고리 선택하기")
+          .customFont(.headlineB)
+        
+        categoryList
+        
+        Spacer()
+      }
+      .padding(.horizontal, 20)
+      
     }
+  }
+}
+
+extension CategoryWaypointView {
+  
+  private var recommenedArea: some View {
+    return VStack(alignment: .leading, spacing: 5) {
+      
+      HStack {
+        Text("오늘의 추천 질문")
+          .customFont(.caption2B)
+          .foregroundStyle(.gray300)
+          .padding(15)
+        Spacer()
+      }
+      
+      Text(categoryWayVM.recommendQuestion.content)
+        .multilineTextAlignment(.leading)
+        .customFont(.headlineB)
+        .foregroundStyle(.white)
+        .padding(15)
+    }
+    .frame(maxWidth: .infinity)
+    .background(
+      RoundedRectangle(cornerRadius: 8)
+        .foregroundStyle(.purple800)
+    )
+  }
+  
+  private var categoryList: some View {
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    
+    // TODO: - OnTapGesture로 SelectionQuestionView로 이동,, 차후구현
+    return LazyVGrid(columns: columns, spacing: 20) {
+      ForEach(Category.allCases, id: \.self) { category in
+        Text(category.type)
+          .customFont(.headlineB)
+          .foregroundColor(.white)
+          .frame(width: 100, height: 100)
+          .background(
+            Image("\(category.rawValue)Img")
+              .resizable()
+              .aspectRatio(contentMode: .fill)
+              .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+          )
+      }
+    }
+  }
 }
 
 #Preview {
-    CategoryWaypointView()
+  CategoryWaypointView()
 }
