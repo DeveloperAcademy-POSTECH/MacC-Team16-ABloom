b560cdb906cedb5df4b861e1fc70f9754f61c486
radin <dlwlsgml8847@gmail.com>
2023-11-21 17:11:43 +0900

    [Feat/#242] 로그인 구현

--- ABloom/ABloom/Presentation/SignIn/SignInViewModel.swift
+++ ABloom/ABloom/Presentation/SignIn/SignInViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  LoginViewModel.swift
+//  SignInViewModel.swift
 //  ABloom
 //
 //  Created by 정승균 on 10/19/23.
@@ -9,30 +9,23 @@ import FirebaseAuth
 import Foundation
 
 @MainActor
-final class LoginViewModel: ObservableObject {
+final class SignInViewModel: ObservableObject {
   @Published var user: AuthDataResultModel? = nil
   @Published var isSignInSuccess = false
-  @Published var showPrivacyPolicy = false
-  @Published var showTermsOfUse = false
+
   @Published var isOldUser = false
-  
-  func privacyPolicyTapped() {
-    showPrivacyPolicy = true
-  }
-  
-  func termsOfUseTapped() {
-    showTermsOfUse = true
-  }
   
   func loadCurrentUser() throws {
     let authDataResult = try AuthenticationManager.shared.getAuthenticatedUser()
     self.user = authDataResult
-    self.isSignInSuccess = true
+    
+    if user != nil {
+      self.isSignInSuccess = true
+    }
   }
   
   /// Apple ID로 로그인합니다.
   func signInApple() async throws {
-    // 유저 로그인
     let helper = SignInAppleHelper()
     let tokens = try await helper.startSignInWithAppleFlow()
     let authDataResult = try await AuthenticationManager.shared.signInWithApple(tokens: tokens)
@@ -47,9 +40,10 @@ final class LoginViewModel: ObservableObject {
   
   func checkOldUser(userId: String) async {
     guard let dbUser = try? await UserManager.shared.getUser(userId: userId) else {
+      isOldUser = false
       return
     }
-    
+
     isOldUser = true
   }
 }
