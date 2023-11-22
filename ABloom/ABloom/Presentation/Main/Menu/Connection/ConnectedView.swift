8c5a1c8a03d7dfa9eedca09969b4111d08ec1242
seunggyun-jeong <jskyun98@gmail.com>
2023-11-22 12:19:00 +0900

    [Feat/#254] 연결 완료 뷰 연동

--- ABloom/ABloom/Presentation/Main/Menu/Connection/ConnectedView.swift
+++ ABloom/ABloom/Presentation/Main/Menu/Connection/ConnectedView.swift
@@ -8,17 +8,64 @@
 import SwiftUI
 
 struct ConnectedView: View {
+  let myCode: String
+  let fianceName: String
+  
+  @State var showToast = false
+  
+  let viewHorizontalPadding: CGFloat = 20
+  var checkCirclePadding: CGFloat {
+    100 - viewHorizontalPadding
+  }
+  
   var body: some View {
-    VStack {
-      Image(systemName: "circle.fill")
-        .resizable()
-        .scaledToFit()
-        .padding(.horizontal, 100)
-        .padding(.bottom, 31)
+    
+    ZStack {
+      VStack {
+        Image("check")
+          .resizable()
+          .scaledToFit()
+          .padding(.horizontal, checkCirclePadding)
+          .padding(.bottom, 31)
+        
+        CopyStrokeInputField(myCode: myCode, alignment: .center, copyAction: copyClipboard)
+          .strokeInputFieldStyle(isValueValid: true, alignment: .center)
+          .padding(.bottom, 31)
+        
+        Text("\(fianceName)님과\n연결되어 있어요.")
+          .customFont(.title3B)
+          .foregroundStyle(.gray600)
+          .multilineTextAlignment(.center)
+      }
+      .padding(.horizontal, viewHorizontalPadding)
+      
+      
+      VStack {
+        Spacer()
+        if showToast {
+          ToastView(message: "코드가 복사되었습니다")
+            .padding(.bottom, 50)
+        }
+      }
     }
   }
 }
 
 #Preview {
-  ConnectedView()
+  ConnectedView(myCode: "9HZI4ZHHjW", fianceName: "최지은")
+}
+
+extension ConnectedView {
+  private func copyClipboard() {
+    UIPasteboard.general.string = myCode
+    withAnimation {
+      showToast.toggle()
+    }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      withAnimation {
+        self.showToast = false
+      }
+    }
+  }
 }
