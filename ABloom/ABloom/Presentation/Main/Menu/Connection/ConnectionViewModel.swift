a27d435a7c1e20117c8690b6706cb84c89b8d0d3
seunggyun-jeong <jskyun98@gmail.com>
2023-11-26 22:51:10 +0900

    [Feat/#279] UserManager에 있는 연결로 수정

--- ABloom/ABloom/Presentation/Main/Menu/Connection/ConnectionViewModel.swift
+++ ABloom/ABloom/Presentation/Main/Menu/Connection/ConnectionViewModel.swift
@@ -33,7 +33,7 @@ final class ConnectionViewModel: ObservableObject {
   func connectUser() {
     Task {
       do {
-        try await ConnectionManager.shared.connectFiance(connectionCode: self.inputText)
+        try await UserManager.shared.connectFiance(connectionCode: self.inputText)
         getUsers()
       } catch let error as ConnectionError {
         self.errorMessage = error.errorMessage()
