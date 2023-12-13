4fc9ea5057a53b7bcd1ae45c611005e89dd9ece9
seunggyun-jeong <jskyun98@gmail.com>
2023-12-13 14:15:06 +0900

    [Feat] 추천질문 트래킹 구현

--- ABloom/ABloom/Resources/Utilities/MixpanelManager.swift
+++ ABloom/ABloom/Resources/Utilities/MixpanelManager.swift
@@ -10,24 +10,19 @@ import Mixpanel
 
 class MixpanelManager {
   static private var instance = Mixpanel.mainInstance()
-  
-  static func example() {
-    instance.track(event: "Signed Up", properties: ["Signup Type": "Referral"])
-  }
-
 }
 
 // MARK: Track SignUp event
 extension MixpanelManager {
   static func signUpStart(pageNum: Int) {
-    let properties: [String: String] = ["onboardingPage":"\(pageNum + 1)"]
+    let properties: [String: String] = ["Onboarding Page":"\(pageNum + 1)"]
     
     instance.track(event: "signup_start", properties: properties)
     print("track")
   }
   
   static func signUpSocial(type: String) {
-    let properties: [String: String] = ["socialLogin":type]
+    let properties: [String: String] = ["Social Login":type]
     
     instance.people.set(properties: properties)
     instance.track(event: "signup_social", properties: properties)
@@ -38,21 +33,21 @@ extension MixpanelManager {
   }
   
   static func signUpSexType(type: UserSexType) {
-    let properties = ["sex":type.rawValue]
+    let properties = ["Sex":type.rawValue]
     
     instance.track(event: "signup_sex_type", properties: properties)
     instance.people.set(properties: properties)
   }
   
   static func signUpDate(date: Date) {
-    let properties = ["marriageDate": date.formatToYMD()]
+    let properties = ["Marriage Date": date.formatToYMD()]
     
     instance.track(event: "signup_date", properties: properties)
     instance.people.set(properties: properties)
   }
   
   static func signUpName(name: String) {
-    let properties = ["name": name]
+    let properties = ["Name": name]
     
     instance.track(event: "signup_name", properties: properties)
     instance.people.set(properties: properties)
@@ -66,7 +61,7 @@ extension MixpanelManager {
 // MARK: Track SignIn event
 extension MixpanelManager {
   static func signIn(type: String) {
-    let properties = ["socialLogin": type]
+    let properties = ["Social Login": type]
     
     instance.track(event: "signin_complete", properties: properties)
   }
@@ -79,27 +74,33 @@ extension MixpanelManager {
   }
   
   static func qnaCategory(category: String) {
-    let properties = ["category": category]
+    let properties = ["Category": category]
     
     instance.track(event: "qna_category", properties: properties)
   }
   
   static func qnaSelectQuestion(questionId: Int) {
-    let properties = ["questionId": "\(questionId)"]
+    let properties = ["Question ID": "\(questionId)"]
     
     instance.track(event: "qna_select_question", properties: properties)
   }
   
   static func qnaAnswer(letterCount: Int) {
-    let eventProperties = ["letterCount": "\(letterCount)"]
+    let eventProperties = ["Letter Count": "\(letterCount)"]
     
     instance.people.increment(property: "answeredQuestion", by: 1)
     instance.track(event: "qna_answer", properties: eventProperties)
   }
   
   static func qnaReaction(type: String) {
-    let properties = ["reactionType": "\(type)"]
+    let properties = ["Reaction Type": "\(type)"]
     
     instance.track(event: "qna_reaction", properties: properties)
+  }
+  
+  static func qnaRecommendedQuestion(category: String, questionId: Int) {
+    let properties = ["Category":category, "Question ID":"\(questionId)"]
+    
+    instance.track(event: "qna_recommended_question", properties: properties)
   }
 }
