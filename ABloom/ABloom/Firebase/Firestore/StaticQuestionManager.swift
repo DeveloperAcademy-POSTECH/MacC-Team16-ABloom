e7e85ad695a15e74b4a244a85046d9736b667bfd
radin <dlwlsgml8847@gmail.com>
2024-06-04 16:50:33 +0900

    [Chore/#361] StaticQuestionManager 코드 삭제, UserManager 코드 삭제 및 deleteUser메서드 로직 수정

--- ABloom/ABloom/Firebase/Firestore/StaticQuestionManager.swift
+++ ABloom/ABloom/Firebase/Firestore/StaticQuestionManager.swift
@@ -72,47 +72,4 @@ final class StaticQuestionManager {
   func getQuestionById(id: Int) async throws -> DBStaticQuestion {
     try await questionCollection.document("\(id)").getDocument(as: DBStaticQuestion.self)
   }
-  
-  
-  // MARK: Will be deprecated properties
-  @Published var essentialQuestionsOrder = [Int]()
-  @Published var essentialQuestionsRandom = [Int]()
-  
-  // MARK: Will be deprecated method
-  // MARK: GET Method
-  func getQuestionsWithoutAnswers(myId: String, fianceId: String?) async throws -> [DBStaticQuestion] {
-    var ids = try await getAnswersId(userId: myId)
-    
-    if let fianceId = fianceId {
-      ids += try await getAnswersId(userId: fianceId)
-    }
-    
-    var allQuestions = try await questionCollection.order(by: "q_id").getDocuments(as: DBStaticQuestion.self)
-    
-    allQuestions.removeAll { question in
-      for id in ids {
-        if id == question.questionID {
-          return true
-        }
-      }
-      return false
-    }
-    
-    return allQuestions
-  }
-  
-  private func getAnswersId(userId: String) async throws -> [Int] {
-    let myAnswers = try await UserManager.shared.getAnswers(userId: userId)
-    
-    return myAnswers.map { answer in answer.questionId }
-  }
-  
-  func getAnsweredQuestions(questionIds: [Int]) async throws -> [DBStaticQuestion] {
-    if questionIds.isEmpty {
-      return []
-    }
-    return try await questionCollection
-      .whereField(DBStaticQuestion.CodingKeys.questionID.rawValue, in: questionIds)
-      .getDocuments(as: DBStaticQuestion.self)
-  }
 }
